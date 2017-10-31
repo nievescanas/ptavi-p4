22bd7829405cbdf2d0b0079f4f93cc750622a474
Nieves Canas Martin <nievescm@alpha29.aulas.gsyc.urjc.es>
2017-10-31 15:35:01 +0100

    Comentarios pep8

--- client.py
+++ client.py
@@ -21,13 +21,12 @@ if sys.argv[3] == 'register':
 elif sys.argv[3] == 'REGISTER':
     for palabra in sys.argv[3:]:
         LINE += ('{p}{espacio}'.format(p=palabra, espacio=' '))
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+"""
+Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+"""
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((sys.argv[1], int(sys.argv[2])))
-    print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
